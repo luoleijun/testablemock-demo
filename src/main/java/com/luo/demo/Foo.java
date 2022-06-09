953078b0130900e5604c218234250a213c9bcd46
luoleijun <49936347+luoleijun@users.noreply.github.com>
2022-06-09 13:57:56 +0800

    Create Foo.java

---
+++ src/main/java/com/luo/demo/Foo.java
@@ -0,0 +1,14 @@
+package com.luo.demo;
+
+/**
+ * @Author leijun.luo
+ * @Date 2022/5/25 17:48
+ * @Version 1.0
+ */
+public class Foo {
+    public static String a() {
+        return StaticMethod.function1(1);
+    }
+
+
+}
