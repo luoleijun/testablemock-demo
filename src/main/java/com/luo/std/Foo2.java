acf0bdb7cb1a948f031ed00a4321839d7f7441bd
luoleijun <49936347+luoleijun@users.noreply.github.com>
2022-06-09 13:59:36 +0800

    Create Foo2.java

---
+++ src/main/java/com/luo/std/Foo2.java
@@ -0,0 +1,13 @@
+package com.luo.std;
+
+/**
+ * @Author leijun.luo
+ * @Date 2022/6/9 10:42
+ * @Version 1.0
+ */
+public class Foo2 {
+
+    public static String b() {
+        return Util.add(1,2);
+    }
+}
